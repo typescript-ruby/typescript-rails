d24913f8628f983fe727beccbabc0f4b64735328
KAWACHI Takashi <kawachi@p-lucky.net>
2012-12-09 20:48:31 +0900

    Added tests for <reference> to .ts and .d.ts

--- test/template_handler_test.rb
+++ test/template_handler_test.rb
@@ -9,6 +9,10 @@ end
 DummyApp = ActionDispatch::Routing::RouteSet.new
 DummyApp.draw do
   get "site/index"
+  get "site/ref1_1"
+  get "site/ref1_2"
+  get "site/ref2_1"
+  get "site/ref2_2"
 end
 
 class TemplateHandlerTest < ActiveSupport::TestCase
@@ -23,4 +27,15 @@ class TemplateHandlerTest < ActiveSupport::TestCase
 
     assert_match "var x = 5;\r\n", last_response.body
   end
+
+  test "<reference> to other .ts file works" do
+    get "/site/ref1_2.js"
+    assert_match "var f = function (x, y) {\r\n    return x + y;\r\n};\r\nf(1, 2);\r\n", last_response.body
+  end
+
+  test "<reference> to other .d.ts file works" do
+    get "/site/ref2_2.js"
+    assert_match "f(1, 2);\r\n", last_response.body
+  end
+
 end
