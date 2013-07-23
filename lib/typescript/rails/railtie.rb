9059fe35086f4352d3658638bcf82c84cc99c1f3
brainling <matthew.allen.holmes@gmail.com>
2013-07-22 22:34:28 -0700

    Updated with two fixes for Rails 4 and Ruby 2.0. Added a check to the sprocket initialization to load if Rails >= 4 Updated the references Regex to play nice with Ruby 2.0 regex and fixed a minor bug causing errors when more than one reference line is provided

--- lib/typescript/rails/railtie.rb
+++ lib/typescript/rails/railtie.rb
@@ -4,7 +4,7 @@ module Typescript
       config.before_initialize do |app|
         require 'typescript-rails'
 
-        if app.config.assets.enabled
+        if ::Rails::VERSION::MAJOR >= 4 || app.config.assets.enabled
           require 'sprockets'
           require 'sprockets/engines'
           Sprockets.register_engine '.ts', Typescript::Rails::TypeScriptTemplate
--- lib/typescript/rails/template_handler.rb
+++ lib/typescript/rails/template_handler.rb
@@ -77,7 +77,7 @@ module Typescript
     def self.replace_relative_references(ts_path, source)
       ts_dir = File.dirname(File.expand_path(ts_path))
       escaped_dir = ts_dir.gsub(/["\\]/, '\\\\\&') # "\"" => "\\\"", '\\' => '\\\\'
-      source.gsub(%r!(^///\s*<reference\s+path=")([^/"][^"]+)("\s*/>)!, '\1' + File.join(escaped_dir, '\2') + '\3')
+      source.gsub(%r!(^///\s*<reference\s+path=")([^"]+)("\s*/>\s*)!) {|m| $1 + File.join(escaped_dir, $2) + $3 }
     end
   end
 end
