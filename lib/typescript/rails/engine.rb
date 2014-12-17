e1d9e36aa4442bbe09b890c76782f8f22db5533c
mhirtie <maxim.hirtie@yopeso.com>
2014-12-17 17:20:18 +0200

    Fixed generator name.

--- lib/typescript/rails/engine.rb
+++ lib/typescript/rails/engine.rb
@@ -1,7 +1,7 @@
 require 'rails/engine'
 
 class Typescript::Rails::Engine < Rails::Engine
-  # config.app_generators.javascript_engine :ts
+  # config.app_generators.javascript_engine :typescript
 
   if config.respond_to?(:annotations)
     config.annotations.register_extensions("ts") { |annotation| /#\s*(#{annotation}):?\s*(.*)$/ }
